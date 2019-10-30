a2f4b586f2633218836be0b0bc0267a0863e7e40
kanuntd <kanun.natthanan@gmail.com>
2019-10-30 17:52:06 +0700

    share

--- app/src/main/java/buu/informatics/s59161081/changmoney/CompareFragment.kt
+++ app/src/main/java/buu/informatics/s59161081/changmoney/CompareFragment.kt
@@ -4,8 +4,10 @@ package buu.informatics.s59161081.changmoney
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.ShareActionProvider
 import androidx.fragment.app.Fragment
 import android.widget.TextView
+import android.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -26,20 +28,37 @@ class CompareFragment : Fragment() {
         binding.favoriteButton.setOnClickListener { view : View ->
             view.findNavController().navigate(R.id.action_compareFragment_to_favoriteFragment)
         }
+
         setHasOptionsMenu(true)
         return binding.root
     }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.options_menu, menu)
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item!!.itemId) {
+            R.id.share -> shareSuccess()
+        }
         return NavigationUI.onNavDestinationSelected(item!!,
             view!!.findNavController())
                 || super.onOptionsItemSelected(item)
     }
 
-
+    private fun getShareIntent() : Intent {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text))
+        return shareIntent
+    }
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
 
 }
